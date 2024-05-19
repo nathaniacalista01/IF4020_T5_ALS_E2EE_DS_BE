6dd7b3d2b134e28e0653b1c34c16c0a87317f451
Nathania Calista <nathania.calista01@gmail.com>
2024-05-19 16:53:39 +0700

    feat : roomchat router

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import { authRouter } from "../controller/auth-controller";
 import { keyMiddleware } from "../middleware/middleware";
+import { roomchatRouter } from "../controller/roomchat-controller";
 
 const router = express.Router();
 router.post("/", keyMiddleware, (req: Request, res: Response) => {});
 
 router.use("/auth", authRouter);
+router.use("/roomchat", roomchatRouter);
 
 export default router;
