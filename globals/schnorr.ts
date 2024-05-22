dae727763a5a3c3c3f817039caf9b3aef33c05eb
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-05-22 18:01:32 +0700

    feat: Use commonly used parameter for schnorr

--- globals/schnorr.ts
+++ globals/schnorr.ts
@@ -1,10 +1,14 @@
 type SchnorrKeys = {
   a: number;
-  p: number;
-  q: number;
+  p: string;
+  q: string;
 };
 
 // Precomputed number of a, p, and q
-let schnorrKeys: SchnorrKeys = { a: 2, p: 383, q: 191 };
+let schnorrKeys: SchnorrKeys = {
+  a: 2,
+  p: '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F',
+  q: '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'
+};
 
 export { schnorrKeys };
